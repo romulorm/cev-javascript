ab55daa5f01f2057cf1086e104989438fed361f6
romulorm <62728349+romulorm@users.noreply.github.com>
2020-05-30 22:10:45 -0300

    ex.01 commit

---
+++ ex.01/ex01.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = window.document.getElementById('mensagem')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 21
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 6 && hora <= 12) {
+        img.src = "imagens/manha.png"
+        document.body.style.background = "#e2cd91"
+    } else if (hora > 12 && hora <= 18) {
+        img.src = "imagens/tarde.png"
+        document.body.style.background = "#b9846f"
+    } else {
+        img.src = "imagens/noite.png"
+        document.body.style.background = "#515154" }
+    }
+    
