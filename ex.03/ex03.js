fe039356d16731e0381d4d0439618ff20868fa3f
romulorm <62728349+romulorm@users.noreply.github.com>
2020-06-02 23:35:11 -0300

    update ex03

--- ex.03/ex03.js
+++ ex.03/ex03.js
@@ -6,12 +6,16 @@ function contar() {
     var f_passo = document.getElementById('passo')
     var passo = f_passo.value
     var res = document.getElementById('dados')
-    if (ini.length == 0 || fim.length == 0 || passo.length == 0) {
-        window.alert('Preencha todos os campos!')
+    if (ini.length == 0 || fim.length == 0 || passo.length == 0 || passo == '0') {
+        window.alert('Preencha os campos corretamente!')
     } else {
-        res.innerHTML = ""
-        for (var c = Number(ini); c <= Number(fim); c+=Number(passo)) {
-            res.innerHTML += `\u{1f449} ${c}`
+        res.innerHTML = 'Resultado: <br>'
+        if (ini < fim) {
+            for (var c = Number(ini); c <= Number(fim); c += Number(passo)) {
+                res.innerHTML += `\u{1f449} ${c}` }
+        } else {
+            for (var c = Number(ini); c >= Number(fim); c -= Number(passo)) {
+                res.innerHTML += `\u{1f449} ${c}` }
         }
         res.innerHTML += `\u{1F6D1}`
     }
