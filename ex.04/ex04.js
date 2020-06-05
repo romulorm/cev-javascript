d795bb3a884df53488e4b04fa455604478553127
romulorm <62728349+romulorm@users.noreply.github.com>
2020-06-04 22:13:15 -0300

    ex04 commit

--- ex.04/ex04.js
+++ ex.04/ex04.js
@@ -1,18 +1,16 @@
-function contar() {
-    var f_inicio = document.getElementById('inicio')
-    var ini = f_inicio.value
-    var f_fim = document.getElementById('fim')
-    var fim = f_fim.value
-    var f_passo = document.getElementById('passo')
-    var passo = f_passo.value
-    var res = document.getElementById('dados')
-    if (ini.length == 0 || fim.length == 0 || passo.length == 0) {
-        window.alert('Preencha todos os campos!')
+function gerarTabuada() {
+    var f_numero = document.getElementById('txtNum')
+    var num = Number(f_numero.value)
+    var tabela = document.getElementById('seltab')
+    tabela.innerHTML = ''
+    if (f_numero.value.length == 0) {
+        window.alert('Por favor digite um número.')
     } else {
-        res.innerHTML = ""
-        for (var c = Number(ini); c <= Number(fim); c+=Number(passo)) {
-            res.innerHTML += `\u{1f449} ${c}`
-        }
-        res.innerHTML += `\u{1F6D1}`
+        for (var c = 1; c <= 10; c++) {
+            var res = num * c
+            var item = document.createElement('option')
+            item.text = `${num} x ${c} = ${res}`
+            tabela.appendChild(item);
+            }
     }
 }
